085c2a75f494f7fc699e272434fa204655082201
TeMecha Newton <tanewton0523@gmail.com>
2020-03-11 21:26:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning"]
   element = "arrays!"
-  my_array.push(element)
+  my_array << element
 end
 
 def add_element_to_start_of_array(array, element)
